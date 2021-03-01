9efdafc0504db32e7f3f087d32dbd0c9db77bffa
Rumsbums27 <felix.waidler@web.de>
2021-03-01 08:29:47 +0100

    Added all setting pictures and finished the main.py

--- README.md
+++ README.md
@@ -7,8 +7,15 @@
 2. Python 3.8 or higher
 3. A Screenshot of your MS-Teams Icon on your Desktop
 4. Screenshots of all Meetings you want to automate
+5. These Screenshot you have to name like `meeting_<hour>-<minute>-<latency>`
 
 ---
 
 You have to put the screenshot of your MS-Teams Icon in the `images` Folder. Then you have to name this `.PNG` File
-to `teams.PNG`.
+to `teams.PNG`. Alternatively you can use the given `teams.PNG`. You also can replace all the settings pictures but this
+could give you an error.
+
+---
+
+You can also check out the [Instagram Account of my class](https://www.instagram.com/9k.ist.abgehoben/)
+or our [Discord Server](https://discord.gg/CZGVAX4)
--- main.py
+++ main.py
@@ -2,32 +2,93 @@ import pyautogui
 import datetime
 import random
 import os
+import time
+
 
 class Teams():
-    def meeting(self, hours=int(), minutes=int(), latency=int(3)):
-        time_latency = list()
-        minutes -= latency
+    def meeting(self):
+        for filename in os.listdir('./images'):
+            if filename.startswith('meeting_'):
+                print(filename)
+                date = filename.split('_')[1]
+                date = date.split('-')
+                time_latency = list()
 
-        for ltnc in range(latency * 2):
-            time_latency.append(minutes + ltnc)
+                hours = int(date[0])
+                minutes = int(date[1])
+                latency = date[2]
+                latency = int(latency.split('.')[0])
 
-        accestime = random.choice(time_latency)
-        time = datetime.time(hour=hours,minute=accestime)
-        time = time.strftime("%H.%M")
+                minutes -= latency
 
-        print(time)
+                for ltnc in range(latency * 2):
+                    time_latency.append(minutes + ltnc)
 
-        while True:
-            now = datetime.datetime.now().strftime("%H.%M")
-            if time == now:
-                picture = pyautogui.locateCenterOnScreen(f'images/teams.PNG')
-                pyautogui.moveTo(picture,duration=2)
-                pyautogui.click(clicks=2,interval=0.1)
-                break
+                accesstime = random.choice(time_latency)
+                start_time = datetime.time(hour=hours, minute=accesstime)
+                start_time = start_time.strftime("%H.%M")
+
+                print(start_time)
+
+                while True:
+                    now = datetime.datetime.now().strftime("%H.%M")
+
+                    if start_time == now:
+                        start_teams = pyautogui.locateCenterOnScreen('images/settings/teams.PNG')
+                        pyautogui.moveTo(start_teams, duration=1)
+                        pyautogui.click(clicks=2, interval=0.1)
+
+                        time.sleep(4)
+
+                        maximise = pyautogui.locateCenterOnScreen('images/settings/maximise.PNG')
+
+                        if maximise is not None:
+                            pyautogui.moveTo(maximise, duration=1)
+                            pyautogui.click()
+
+                            time.sleep(3)
+
+                        calendar = pyautogui.locateCenterOnScreen('images/settings/calendar.PNG')
+
+                        if calendar is not None:
+                            pyautogui.moveTo(calendar, duration=1)
+                            pyautogui.click()
+
+                            time.sleep(3)
+
+                        file = pyautogui.locateCenterOnScreen(f'images/{filename}')
+                        if file is not None:
+                            pyautogui.moveTo(file, duration=1)
+                            pyautogui.click()
+
+                            time.sleep(3)
+
+                        access = pyautogui.locateCenterOnScreen('images/settings/access.PNG')
+                        pyautogui.moveTo(access, duration=1)
+                        pyautogui.click()
+
+                        time.sleep(3)
+
+                        cam = pyautogui.locateCenterOnScreen('images/settings/cam.PNG')
+                        mic = pyautogui.locateCenterOnScreen('images/settings/mic.PNG')
+                        if cam:
+                            pyautogui.moveTo(cam, duration=1)
+                            pyautogui.click()
+
+                            time.sleep(3)
+
+                        if mic:
+                            pyautogui.moveTo(mic, duration=1)
+                            pyautogui.click()
+
+                            time.sleep(3)
+
+                        now_access = pyautogui.locateCenterOnScreen('images/settings/now_access.PNG')
+                        pyautogui.moveTo(now_access, duration=1)
+                        pyautogui.click()
+                        break
+
 
 if __name__ == '__main__':
     teams = Teams()
-    hour = pyautogui.prompt(title='Hour',text='Please type the hour: ',default=0)
-    minute = pyautogui.prompt(title='Minute', text='Please type the minute: ', default=0)
-    latency = pyautogui.prompt(title='Latency', text='Please type the latency: ', default=3)
-    teams.meeting(int(hour), int(minute), int(latency))
+    teams.meeting()
